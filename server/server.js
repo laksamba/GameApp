98f72f754aa93c1ddd076ce1ebbb0f76deecdba8
Sandesh laksamba <sandeshlaksamba123@gmail.com>
2024-06-29 16:09:34 +0545

    file change

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // Allow requests from frontend origin
 const corsOptions = {
-    origin: [ 'http://localhost:5173','https://game-app-ten-vert.vercel.app',],
+    origin: [ "https://game-app-ten-vert.vercel.app"],
     methods: ["POST","GET","PUT","DELETE"],
     CredentialS : true
   };
